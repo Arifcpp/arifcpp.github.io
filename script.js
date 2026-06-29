9d0082ef7bafdc01a7e916964e6ada0b24cac9f5
Arif <134702175+Arifcpp@users.noreply.github.com>
2026-06-29 04:11:56 +0400

    js
    Added a typing animation for various roles using Typed.js.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+var WriteAnim =new Typed('#yazi-anim',{
 
+    strings: [
+        'Tech Enthusiast', 
+        'Game Developer', 
+        'Android Developer',       
+        'Hardware Enthusiast'
+    ],
+
+    typeSpeed: 60, /* Yazılma sürəti */
+    backSpeed: 40,  /* Silinmə sürəti */
+    backDelay :1500,  /* Ekranda gözləmə müddəti (1.5 saniyə) */
+    loop: true  /* Sonsuz dövr etsin */
+
+
+})
